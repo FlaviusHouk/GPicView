f12feb482174502f15d7c9d0bbdf17bcac124268
Vladyslav Stovmanenko <flaviusglamfenix@gmail.com>
2020-03-22 13:34:37 +0200

    Cleaning ptk-menu.c

--- src/ptk-menu.c
+++ src/ptk-menu.c
@@ -18,10 +18,10 @@ GtkWidget* ptk_menu_new_from_data( PtkMenuItemEntry* entries,
                                    gpointer cb_data,
                                    GtkAccelGroup* accel_group )
 {
-  GtkWidget* menu;
-  menu = gtk_menu_new();
-  ptk_menu_add_items_from_data( menu, entries, cb_data, accel_group );
-  return menu;
+    GtkWidget* menu;
+    menu = gtk_menu_new();
+    ptk_menu_add_items_from_data( menu, entries, cb_data, accel_group );
+    return menu;
 }
 
 void ptk_menu_add_items_from_data( GtkWidget* menu,
@@ -29,72 +29,97 @@ void ptk_menu_add_items_from_data( GtkWidget* menu,
                                    gpointer cb_data,
                                    GtkAccelGroup* accel_group )
 {
-  PtkMenuItemEntry* ent;
-  GtkWidget* menu_item = NULL;
-  GtkWidget* sub_menu;
-  GtkWidget* image;
-  GSList* radio_group = NULL;
-  const char* signal;
+    PtkMenuItemEntry* ent;
+    GtkWidget* menu_item = NULL;
+    GtkWidget* sub_menu;
+    GtkWidget* image;
+    GSList* radio_group = NULL;
+    const char* signal;
+    gboolean accel_set;
 
-  for( ent = entries; ; ++ent )
-  {
-    if( G_LIKELY( ent->label ) )
+    for( ent = entries; ; ++ent )
     {
-      /* Stock item */
-      signal = "activate";
-      if( G_UNLIKELY( PTK_IS_STOCK_ITEM(ent) ) )  {
-        menu_item = gtk_image_menu_item_new_from_stock( ent->label, accel_group );
-      }
-      else if( G_LIKELY(ent->stock_icon) )  {
-        if( G_LIKELY( ent->stock_icon > (char *)2 ) )  {
-          menu_item = gtk_image_menu_item_new_with_mnemonic(_(ent->label));
-          if( g_str_has_prefix(ent->stock_icon, "gtk-") )
-            image = gtk_image_new_from_stock( ent->stock_icon, GTK_ICON_SIZE_MENU );
-          else
-            image = gtk_image_new_from_icon_name( ent->stock_icon, GTK_ICON_SIZE_MENU );
-          gtk_image_menu_item_set_image( GTK_IMAGE_MENU_ITEM(menu_item), image );
-        }
-        else if( G_UNLIKELY( PTK_IS_CHECK_MENU_ITEM(ent) ) )  {
-          menu_item = gtk_check_menu_item_new_with_mnemonic(_(ent->label));
-          signal = "toggled";
-        }
-        else if( G_UNLIKELY( PTK_IS_RADIO_MENU_ITEM(ent) ) )  {
-          menu_item = gtk_radio_menu_item_new_with_mnemonic( radio_group, _(ent->label) );
-          if( G_LIKELY( PTK_IS_RADIO_MENU_ITEM( (ent + 1) ) ) )
-            radio_group = gtk_radio_menu_item_get_group( GTK_RADIO_MENU_ITEM(menu_item) );
-          else
-            radio_group = NULL;
-          signal = "toggled";
-        }
-      }
-      else  {
-        menu_item = gtk_menu_item_new_with_mnemonic(_(ent->label));
-      }
+        if( G_LIKELY( ent->label ) )
+        {
+            /* Stock item */
+            signal = "activate";
+            if( G_LIKELY(ent->stock_icon) )  
+            {
+                if( G_LIKELY( ent->stock_icon > (char *)2 ) )  
+                {      
+                    menu_item = gtk_menu_item_new();
 
-      if( G_LIKELY(accel_group) && ent->key ) {
-        gtk_widget_add_accelerator (menu_item, "activate", accel_group,
-                                    ent->key, ent->mod, GTK_ACCEL_VISIBLE);
-      }
+                    image = gtk_image_new_from_icon_name( ent->stock_icon, GTK_ICON_SIZE_MENU );
 
-      if( G_LIKELY(ent->callback) )  { /* Callback */
-        g_signal_connect( menu_item, signal, ent->callback, cb_data);
-      }
+                    GtkWidget* container = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 0);
+                    gtk_container_add(GTK_CONTAINER(container), image);
+                    
+                    GtkWidget* label = gtk_label_new(NULL);
+                    gtk_label_set_text_with_mnemonic(GTK_LABEL(label), ent->label);
+                    gtk_container_add(GTK_CONTAINER(container), label);
 
-      if( G_UNLIKELY( ent->sub_menu ) )  { /* Sub menu */
-        sub_menu = ptk_menu_new_from_data( ent->sub_menu, cb_data, accel_group );
-        gtk_menu_item_set_submenu( GTK_MENU_ITEM(menu_item), sub_menu );
-      }
+                    gtk_container_add(GTK_CONTAINER(menu_item), container);
+                }
+                else if( G_UNLIKELY( PTK_IS_CHECK_MENU_ITEM(ent) ) )  
+                {
+                    menu_item = gtk_check_menu_item_new_with_mnemonic(_(ent->label));
+                    signal = "toggled";
+                }
+                else if( G_UNLIKELY( PTK_IS_RADIO_MENU_ITEM(ent) ) ) 
+                {
+                    menu_item = gtk_radio_menu_item_new_with_mnemonic( radio_group, _(ent->label) );
+                  
+                    if( G_LIKELY( PTK_IS_RADIO_MENU_ITEM( (ent + 1) ) ) )
+                        radio_group = gtk_radio_menu_item_get_group( GTK_RADIO_MENU_ITEM(menu_item) );
+                    else
+                        radio_group = NULL;
+                    
+                    signal = "toggled";
+                }
+            }
+            else  
+            {
+                menu_item = gtk_menu_item_new_with_mnemonic(_(ent->label));
+            }
+
+            if( G_LIKELY(accel_group) && ent->key ) 
+            {
+                gtk_widget_add_accelerator (menu_item, 
+                                            "activate", 
+                                            accel_group,
+                                            ent->key,
+                                            ent->mod,
+                                            GTK_ACCEL_VISIBLE);
+            }
+
+            /* Callback */
+            if( G_LIKELY(ent->callback) )  
+            { 
+                g_signal_connect( menu_item, signal, ent->callback, cb_data);
+            }
+
+            /* Sub menu */
+            if( G_UNLIKELY( ent->sub_menu ) )  
+            { 
+                sub_menu = ptk_menu_new_from_data( ent->sub_menu, cb_data, accel_group );
+                gtk_menu_item_set_submenu( GTK_MENU_ITEM(menu_item), sub_menu );
+            }
+        }
+        else
+        {
+            if( ! ent->stock_icon ) /* End of menu */
+                break;
+
+            menu_item = gtk_separator_menu_item_new();
+        }
+
+        gtk_menu_shell_append ( GTK_MENU_SHELL(menu), menu_item );
+
+        /* Return */
+        if( G_UNLIKELY(ent->ret) ) 
+        {
+            *ent->ret = menu_item;
+            ent->ret = NULL;
+        }
     }
-    else
-    {
-      if( ! ent->stock_icon ) /* End of menu */
-        break;
-      menu_item = gtk_separator_menu_item_new();
-    }
-    gtk_menu_shell_append ( GTK_MENU_SHELL(menu), menu_item );
-    if( G_UNLIKELY(ent->ret) ) {/* Return */
-      *ent->ret = menu_item;
-      ent->ret = NULL;
-    }
-  }
 }
